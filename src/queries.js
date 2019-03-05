d24f676ed58cd861836ad3b52b00aa488334e545
eunsukimme <eunsu.dev@gmail.com>
2019-03-05 14:28:01 +0900

    Setup GraphQL client module(graphql-tag)

---
+++ src/queries.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+
+export const HOME_PAGE = gql`
+    {
+        movies(limit: 20, rating: 7){
+            title
+            genre
+            summary
+        }
+    }
+`;
