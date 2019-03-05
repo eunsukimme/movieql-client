cc699629233ab0f67b900b9880ab9c33fc8b0db1
eunsukimme <eunsu.dev@gmail.com>
2019-03-05 14:12:10 +0900

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import client from './apolloClient';
+import Home from './Home';
+import Details from './Details';
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App"></div>
+      <Router>
+        <div className="App">
+          <Route exact={true} path='/' component={Home} />
+          <Route path='/details/:movieId' component={Details} />
+        </div>
+      </Router>
       </ApolloProvider>
     );
   }
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const details = () => "Details";
+
+export default details;
